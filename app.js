6ccfe6fe637806ca57682bfdcc2b56c2d850eac6
Raphael Parree <rparree@edc4it.com>
2018-11-08 07:02:51 +0100

    bumped up versions

--- app.js
+++ app.js
@@ -9,12 +9,12 @@ const recipeApi = require('./routes/recipe-api');
 
 const app = express();
 
-var allowCrossDomain = function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
+const allowCrossDomain = function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
 
-  next();
+    next();
 };
 
 
